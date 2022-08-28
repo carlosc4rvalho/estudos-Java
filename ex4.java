b1d327856ca4b5ea955c3e30ad2a8f71399c8d10
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 11:24:29 -0300

    Update ex4.java

--- ex4.java
+++ ex4.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+// Construir um programa em Java que leia dois números e efetue a adição. Caso
+// o valor somado seja maior que 20, este deverá ser apresentado somando-se a
+// ele mais 8; caso o valor somado seja menor ou igual a 20, este deverá ser
+// apresentado subtraindo-se 5.
+
 public class ex4 {
 
 	public static void main(String[] args) {
