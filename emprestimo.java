82c2d46c45e7a2edb3b885d69dea56be25d70621
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 11:16:41 -0300

    Update and rename ex11.java to emprestimo.java

--- emprestimo.java
+++ emprestimo.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+// A prefeitura de Contagem abriu uma linha de crédito para os funcionários
+// estatutários. O valor máximo da prestação não poderá ultrapassar 30% do
+// salário bruto. Fazer um algoritmo que permita entrar com o salário bruto e o
+// valor da prestação, e informar se o empréstimo pode ou não ser concedido.
+	
 public class ex11 {
 
 	public static void main(String[] args) {
